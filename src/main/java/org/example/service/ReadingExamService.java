45f4654323863a2c8bb223fb7d039c0172dcd2f2
mateidiana <mateidiana10@gmail.com>
2024-12-16 10:37:01 +0200

    added service for exams

--- src/main/java/org/example/service/ReadingExamService.java
+++ src/main/java/org/example/service/ReadingExamService.java
@@ -97,10 +97,110 @@ public class ReadingExamService {
         studentRepo.update(student);
     }
 
-    //show all reading exams
-    //show results of all students on all reading exams of a teacher
-    //delete reading exam
-    //create/update reading exam
-    //filter students by passing grade
+    public List<ReadingExam> showAllReadingExams(){
+        return readingExamRepo.getAll();
+    }
+
+    public List<ReadingExam> examsOfATeacher(int teacherId){
+        List<ReadingExam> exams=new ArrayList<>();
+        for (ReadingExam exam:readingExamRepo.getAll())
+            if (exam.getTeacher()==teacherId)
+                exams.add(exam);
+        return exams;
+    }
+
+    //show results of all students on a reading exam of a teacher
+    public List<ExamResult> showAllResultsOfTeacherExam(int teacherId, int examId){
+        List<ExamResult> allReadingResults=new ArrayList<>();
+        if (getReadingExamById(examId).getTeacher()==teacherId){
+            for (Student student:studentRepo.getAll())
+                for (ExamResult result:student.getResults())
+                    if(result.getExam()==examId)
+                        allReadingResults.add(result);
+        }
+        return allReadingResults;
+
+    }
+
+    public boolean removeReadingExam(int examId, int teacherId) {
+        ReadingExam exam = getReadingExamById(examId);
+        if (exam.getTeacher()==teacherId){
+            readingExamRepo.delete(examId);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void createOrUpdateReadingExam(int examId, int teacherId, String examName, int exerciseSet){
+        int found=0;
+        for (ReadingExam exam: readingExamRepo.getAll()){
+            if (exam.getId()==examId)
+            {
+                found=1;
+                updateReadingExam(examId,teacherId,examName,exerciseSet);
+                return;
+            }
+        }
+        if (found==0){
+            createReadingExam(examId,teacherId,examName,exerciseSet);
+        }
+    }
+
+    public void createReadingExam(int examId, int teacherId, String examName, int exerciseSet){
+        ReadingExam e1=new ReadingExam(examId,examName,teacherId);
+        readingExamRepo.create(e1);
+        if(exerciseSet==1)
+        {
+            int nextId=questionRepo.getAll().size();
+            Question q1=new Question(nextId,"Der Diener kann auf alle Fragen des Ich-Erzählers antworten.","falsch");
+            questionRepo.create(q1);
+            q1.setReadingExamId(examId);
+            questionRepo.update(q1);
+            List<Question> questions=new ArrayList<>();
+            questions.add(q1);
+            e1.setExercises(questions);
+            e1.setText("Ich befahl mein Pferd aus dem Stall zu holen. Der Diener verstand mich nicht.\nIch ging selbst in den Stall, sattelte mein Pferd und bestieg es. In der Ferne hörte ich eine Trompete blasen,\nich fragte ihn, was das bedeute. Er wusste nichts und hatte nichts gehört. Beim Tore hielt er mich auf und fragte:\n\"Wohin reitest du, Herr?\" \"Ich weiß es nicht,\" sagte ich, \"nur weg von hier. Immerfort weg von hier, nur so kann ich\nmein Ziel erreichen.\" \"Du kennst also dein Ziel?\" fragte er. \"Ja,\" antwortete ich, \"ich sagte es doch: »Weg-von-hier«,\ndas ist mein Ziel.\" \"Du hast keinen Essvorrat mit,\" sagte er. \"Ich brauche keinen,\" sagte ich, \"die Reise ist so lang,\ndass ich verhungern muss, wenn ich auf dem Weg nichts bekomme. Kein Essvorrat kann mich retten. Es ist ja zum Glück eine\nwahrhaft ungeheure Reise.\"");
+
+        }
+        e1.setTextTitle("Der Aufbruch");
+        e1.setTextAuthor("Franz Kafka");
+        readingExamRepo.update(e1);
+    }
+
+    public void updateReadingExam(int examId, int teacherId,String courseName, int exerciseSet){
+        ReadingExam exam=getReadingExamById(examId);
+
+        exam.setExamName(courseName);
+        exam.setTeacher(teacherId);
+
+        if(exerciseSet==1)
+        {
+            int nextId=questionRepo.getAll().size();
+            Question q1=new Question(nextId,"Der Diener kann auf alle Fragen des Ich-Erzählers antworten.","falsch");
+            questionRepo.create(q1);
+            q1.setReadingId(examId);
+            questionRepo.update(q1);
+
+            List<Question> questions=new ArrayList<>();
+            questions.add(q1);
+            exam.setExercises(questions);
+            exam.setText("Ich befahl mein Pferd aus dem Stall zu holen. Der Diener verstand mich nicht.\nIch ging selbst in den Stall, sattelte mein Pferd und bestieg es. In der Ferne hörte ich eine Trompete blasen,\nich fragte ihn, was das bedeute. Er wusste nichts und hatte nichts gehört. Beim Tore hielt er mich auf und fragte:\n\"Wohin reitest du, Herr?\" \"Ich weiß es nicht,\" sagte ich, \"nur weg von hier. Immerfort weg von hier, nur so kann ich\nmein Ziel erreichen.\" \"Du kennst also dein Ziel?\" fragte er. \"Ja,\" antwortete ich, \"ich sagte es doch: »Weg-von-hier«,\ndas ist mein Ziel.\" \"Du hast keinen Essvorrat mit,\" sagte er. \"Ich brauche keinen,\" sagte ich, \"die Reise ist so lang,\ndass ich verhungern muss, wenn ich auf dem Weg nichts bekomme. Kein Essvorrat kann mich retten. Es ist ja zum Glück eine\nwahrhaft ungeheure Reise.\"");
+            exam.setTextTitle("Der Aufbruch");
+            exam.setTextAuthor("Franz Kafka");
+        }
+        readingExamRepo.update(exam);
+    }
+
+
+    public List<Student> filterStudentsByPassingGradeOnReadingExam(int examId){
+        List<Student> filteredStud=new ArrayList<>();
+        for (Student stud:studentRepo.getAll())
+            for (ExamResult result:stud.getResults())
+                if (result.getExam()==examId)
+                    if (result.getResult()>=5.0)
+                        filteredStud.add(stud);
+        return filteredStud;
+    }
 
 }
